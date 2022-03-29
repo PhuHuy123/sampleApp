1b3fd430cd9f29d1d794649c618e9b99f785ebce
PhuHuy123 <nguyenphuhuy180920@gmail.com>
2022-03-29 15:43:31 +0700

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # get 'users/new'
+  root  'static_pages#home'
+  get 'help'  => 'static_pages#help'
+  get 'about'  => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+end
